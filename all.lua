c5000cc59d347856c1b3c8886f112ac9a7bff6dc
LoiSprite <159018964+LoiSprite@users.noreply.github.com>
2025-09-16 00:59:38 +0700

    Create all.lua

---
+++ all.lua
@@ -0,0 +1,15 @@
+spawn(function()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/LoiSprite/Map-dit/refs/heads/main/Config%20Steal.txt'))()
+end)
+
+spawn(function()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/LoiSprite/Map-dit/refs/heads/main/HeoBeo.lua'))()
+end)
+
+spawn(function()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/LoiSprite/Map-dit/refs/heads/main/LockFPS.lua'))()
+end)
+
+spawn(function()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/LoiSprite/Map-dit/refs/heads/main/TrackStats.lua'))()
+end)
