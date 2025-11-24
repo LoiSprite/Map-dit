3f49f68c1e5191bdb01dde1d62ff5a09c698f629
LoiSprite <159018964+LoiSprite@users.noreply.github.com>
2025-11-24 20:10:28 +0700

    Update TrackStats.lua

--- TrackStats.lua
+++ TrackStats.lua
@@ -1,6 +1,3 @@
 repeat task.wait() until game:IsLoaded()
-local Players = game:GetService("Players")
-local LocalPlayer = Players.LocalPlayer or Players.PlayerAdded:Wait()
-LocalPlayer.CharacterAdded:Wait()
 getgenv().PET_TRACKER_KEY = "loi2005"
 loadstring(game:HttpGet("https://tracksab.com/trackgameantrom/main.txt"))()
