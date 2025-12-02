95f0ce3b199e3209242639c2e597b63823803990
LoiSprite <159018964+LoiSprite@users.noreply.github.com>
2025-12-02 15:21:59 +0700

    Update TrackStatsChim.lua

--- TrackStatsChim.lua
+++ TrackStatsChim.lua
@@ -1,7 +1,1 @@
-getgenv().Setting = {
-    UID = '7D38D81607A9BEABBD74645707252FE1',
-    DelayUpdate = 120,
-    Note = 'loicoca'
-}
-API_V2 = true -- false
-loadstring(game:HttpGet('https://raw.githubusercontent.com/seian0729/ngu/refs/heads/master/client-2ngon.lua'))()
+loadstring(game:HttpGet("https://tracksab.com/trackgameantrom/claim.txt"))()
